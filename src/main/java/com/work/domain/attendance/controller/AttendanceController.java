8da2596a860dfc4560f764f14fc1010ba054f863
lakelol1990 <mamber0243@naver.com>
2026-02-24 17:58:07 +0900

    fix: controller URI 수정

--- src/main/java/com/work/domain/attendance/controller/AttendanceController.java
+++ src/main/java/com/work/domain/attendance/controller/AttendanceController.java
@@ -11,37 +11,37 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/attendance09999999")
+@RequestMapping("/api/attendance")
 @RequiredArgsConstructor
 public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
-    /** 현재 로그인 userId 가져오기 */
+    /** 현재 로그인 userId 가져오는거 입니다.*/
     private Long getUserId(){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return Long.parseLong(auth.getName());
     }
 
-    /** 출근 */
+    /** 출근 입니다. */
     @PostMapping("/check-in")
     public void checkIn(){
         attendanceService.checkIn(getUserId());
     }
 
-    /** 퇴근 */
+    /** 퇴근 입니다. */
     @PostMapping("/check-out")
     public void checkOut(){
         attendanceService.checkOut(getUserId());
     }
 
-    /** 오늘 기록 */
+    /** 오늘 기록 입니다. */
     @GetMapping("/today")
     public AttendanceDto today(){
         return attendanceService.getToday(getUserId());
     }
 
-    /** 내 전체 기록 */
+    /** 내 전체 기록 입니다.  */
     @GetMapping("/my")
     public List<AttendanceDto> my(){
         return attendanceService.getMyAll(getUserId());
